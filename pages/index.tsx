c2dbe1d5e6996026aabe5bb70353e3ed485fe11d
oliveirafc10 <oliveirafc1@gmail.com>
2022-03-15 07:14:12 -0300

    front_next

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+export default function Home () {
+  return (
+    <div>
+      <Head>
+        <title>Tupã Wash</title>
+      </Head>
+
+      <main>
+        <h1>Bem vindo a Tupã Wash</h1>
+      </main>
+
+    </div>
+  )
+}
