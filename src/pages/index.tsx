873c2e2cb091b21cd49e117f4f51b5036b59c613
oliveirafc10 <oliveirafc1@gmail.com>
2022-03-25 12:57:42 -0300

    celular

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,16 +2,20 @@ import React from 'react';
 import Head from 'next/head';
 
 import Logo from '../assets/logo.svg'
-import { Container } from '../styles/pages/Home';
+import { Container, Header } from '../styles/pages/Home';
 
 const Home: React.FC = () => {
   return (
     <Container>
-      <Head>
+      <head>
         <title>Tupã Wash | Home</title>
-      </Head>
+      </head>
 
-      <Logo />
+      <Header>
+        <Logo />
+        <ul>Estética automotiva em Tupã</ul>
+      </Header>
+
       <h1>Bem vindo a Tupã Wash</h1>
       <p>Seu carro novo de novo</p>
     </Container>
--- src/styles/pages/Home.ts
+++ src/styles/pages/Home.ts
@@ -5,20 +5,59 @@ export const Container = styled.div`
   height: 100vh;
 
   display: flex;
-  justify-content: center;
+  justify-content:  border
   align-items: center;
   flex-direction: column;
+  margin: 10px;
 
   h1 {
     font-size: 54px;
     color: ${props => props.theme.colors.primary};
     margin-top: 5px;
-
   }
 
   p {
     margin-top: 24px;
     font-size: 24px;
     line-height: 32px;
+    margin: 20px;
   }
+
+  @media (max-width: 699px) {
+      margin: 0;
+      width: 100%;
+      padding: 0.8 rem;
+    }
+
 `;
+
+export const Header = styled.header `
+
+  display: flex;
+  justify-content: border;
+  margin: 20px;
+
+  ul {
+  color: ${props => props.theme.colors.text};
+  margin-left: 50px;
+  margin-top: 20px;
+  font-size: 24px;
+  line-height: 32px;
+
+}
+@media (max-width: 699px) {
+  margin: 0;
+  width: 100%;
+  padding: 0.8 rem;
+
+  display: grid;
+  justify-content: center;
+
+  ul {
+    margin-top: 0,6 rem;
+    margin-left: 0;
+  }
+}
+
+`;
+
